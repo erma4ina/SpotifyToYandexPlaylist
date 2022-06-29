9644e8754d0253f47d36ac9c6901883eb748e58d
erma4ina <102543402+erma4ina@users.noreply.github.com>
2022-06-29 13:05:59 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$json = file_get_contents(__DIR__ . '/YourLibrary.json');
+
+$data = json_decode($json, true);
+
+const RESULTS_PATH = __DIR__ . '/results.txt';
+
+foreach ($data['tracks'] as $index => $trackArray) {
+    file_put_contents(RESULTS_PATH, $trackArray['artist'] . ' - ' . $trackArray['track'] . PHP_EOL, FILE_APPEND);
+}
+
+foreach ($data['albums'] as $index => $albumArray) {
+    file_put_contents(RESULTS_PATH, $albumArray['artist'] . ' - ' . $albumArray['album'] . PHP_EOL, FILE_APPEND);
+}
+
+
