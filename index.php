178aafe2fbed0df3e84f3c28a2dc93c9260ce88c
erma4ina <ermoliano@gmail.com>
2022-07-01 16:22:14 +0300

    Косметические правки + добавляю .gitignore

--- index.php
+++ index.php
@@ -3,7 +3,9 @@ $json = file_get_contents(__DIR__ . '/YourLibrary.json');
 
 $data = json_decode($json, true);
 
-const RESULTS_PATH = __DIR__ . '/results.txt';
+const RESULTS_PATH = __DIR__ . '/outputListForYandexMusic.txt';
+
+file_put_contents('outputListForYandexMusic.txt', '');
 
 foreach ($data['tracks'] as $index => $trackArray) {
     file_put_contents(RESULTS_PATH, $trackArray['artist'] . ' - ' . $trackArray['track'] . PHP_EOL, FILE_APPEND);
@@ -12,5 +14,3 @@ foreach ($data['tracks'] as $index => $trackArray) {
 foreach ($data['albums'] as $index => $albumArray) {
     file_put_contents(RESULTS_PATH, $albumArray['artist'] . ' - ' . $albumArray['album'] . PHP_EOL, FILE_APPEND);
 }
-
-
